e03f0a17aaa73e093f32945f24bdb088e79da0ee
Isaiah Odhner <isaiahodhner@gmail.com>
2026-04-29 06:32:24 -0400

    tweak

--- src/game.js
+++ src/game.js
@@ -1144,14 +1144,14 @@ function initRendering() {
 	if (renderer === webGLRenderer && theme !== "perf" && theme !== "wireframe") {
 		saoPass = new SAOPass(scene, camera, false, false, new THREE.Vector2(window.innerWidth, window.innerHeight));
 		saoPass.params.output = SAOPass.OUTPUT.Default;
-		saoPass.params.saoBias = 0.4;
-		saoPass.params.saoIntensity = 0.0022;
-		saoPass.params.saoScale = 1;
+		// saoPass.params.saoBias = 0.4;
+		saoPass.params.saoIntensity = 0.003;
+		// saoPass.params.saoScale = 1;
 		saoPass.params.saoKernelRadius = 9;
 		saoPass.params.saoMinResolution = 0.005;
-		saoPass.params.saoBlur = true;
-		saoPass.params.saoBlurRadius = 6;
-		saoPass.params.saoBlurStdDev = 4;
+		// saoPass.params.saoBlur = true;
+		// saoPass.params.saoBlurRadius = 6;
+		// saoPass.params.saoBlurStdDev = 4;
 		saoPass.params.saoBlurDepthCutoff = 0.02;
 		saoPass.renderToScreen = false;
 	}
